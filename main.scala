385d102b36a95a4ce57ab13c2324af7f3a8ea812
Yi-Lin Tsai <tsaiian@gmail.com>
2014-06-08 14:16:12 +0800

    Read file from training data

--- main.scala
+++ main.scala
@@ -1,5 +1,6 @@
 import scala.math
 import scala.collection.mutable._
+import scala.io.Source
 
 case class MyDouble(a:Double, i:Integer = 0)
 {
@@ -16,61 +17,72 @@ case class MyDouble(a:Double, i:Integer = 0)
 
 object Main extends App
 {
-	val trnData = new ListBuffer[ListBuffer[Double]]
+	//val trnData = new ListBuffer[ListBuffer[Double]]
 	val testdata = new ListBuffer[ListBuffer[Double]]
 
-	val typeCount = 2
-	val featureCount = 3
+	val trnData = Source.fromFile("test.txt" ).getLines.toList.map(x => x.split("\t").toList)
 
-	trnData += (ListBuffer(1,(6),(180),(12)))
-	trnData += (ListBuffer(1,(5.92),(190),(11)))
-	trnData += (ListBuffer(1,(5.58),(170),(12)))
-	trnData += (ListBuffer(1,(5.92),(165),(10)))
-	trnData += (ListBuffer(0,(5),(100),(6)))
-	trnData += (ListBuffer(0,(5.5),(150),(8)))
-	trnData += (ListBuffer(0,(5.42),(130),(7)))
-	trnData += (ListBuffer(0,(5.75),(150),(9)))
+	val typeCount = 2//!!!!!!??
+	val featureCount = trnData(0).length - 1
+
 
 	val meanList = ListBuffer[Double]()
 	val varianceList = ListBuffer[Double]()
 
-	for((category,array) <- trnData.groupBy(_(0)))
+	val dictionary = trnData.groupBy(_(0))
+
+	for((category,array) <- dictionary)
 	{
 		var fIndex = 0
-		for( fIndex <- 1 to featureCount)
+		for( fIndex <- 0 to featureCount - 1)
 		{
-			var fArray = array.flatMap(f => extractIndex(f,fIndex))
+			var fArray = array.flatMap(f => extractIndex(stringList2DoubleList(f.tail), fIndex))
 			var mean = fArray.sum / fArray.length
-			var variance = fArray.flatMap(f=>div(f,mean)).sum/(fArray.length-1)
+			var variance = fArray.flatMap(f=>div(f,mean)).sum/(fArray.length - 1)
 
 			meanList += mean
 			varianceList += variance
 		}
 	}
+	println("END")
+
+
+	def stringList2DoubleList(il: List[String]): List[Double] = il.map(_.toDouble)
 
 	/////////////test/////////////////////
 	
-	val inputList = List(6, 130, 8)
+	val inputList = List(450, 130, 10)
 
 	def loop_getTypePro(n:Integer, t:Integer,  i:MyDouble):MyDouble = 
 	{
 		val index = n + t * featureCount
-		val m = MyDouble(getP(meanList(index), varianceList(index), inputList(n)), typeCount - t - 1)
+		val m = MyDouble(getP(meanList(index), varianceList(index), inputList(n)), t)
+
 
 		if(n >= 1)
 			loop_getTypePro(n - 1, t, i.mul(m))
 		else
 			i.mul(m)
-
-
 	}
 
 	def loop_findMax(n:Integer, i:MyDouble):MyDouble = 
 	{
-		val m = loop_getTypePro(featureCount - 1, n, MyDouble(1.0, typeCount - n - 1))
+		val m = loop_getTypePro(featureCount - 1, n, MyDouble(1.0, n))
 
-		println("~~~" + m.i)
-		println("!!!!!" + (m.a / 2))
+/*
+		var p = 0
+		for((category,array) <- dictionary)
+		{
+			if(p == m.i)
+			{
+				println("" + category)
+				println("P" + (m.a / 2))//!!!
+			}
+
+			p = p + 1
+		}
+*/
+
 
 		if(n >= 1)
 			loop_findMax(n - 1, i.max(m))
@@ -79,17 +91,37 @@ object Main extends App
 
 	}
 
+	def getactualTypeName(m:MyDouble):String = 
+	{
+		var p = 0
+		var result = ""
+		for((category,array) <- dictionary)
+		{
+			if(p == m.i)
+			{
+				println("" + category)
+				println("P" + (m.a / 2))
+
+				result = category
+			}
+			p = p + 1
+		}
+		result
+	}
 	def getP(m:Double, v:Double, x:Double): Double =
 	{
 		val a = 1 / math.sqrt(2.0 * math.Pi * v)
 		val b = math.exp((-1) * (x - m) * (x - m) / (2 * v))
 
-		a * b
+		if(v != 0.0)
+			a * b
+		else
+			50
 	}
-	def extractIndex(v:ListBuffer[Double], i:Int) = List(v(i))
+	def extractIndex(v:List[Double], i:Int) = List(v(i))
 	def div(next:Double,mean:Double) = List( (next-mean)*(next-mean))
 
 	val ttt = loop_findMax(typeCount - 1, MyDouble(Double.MinValue))
-	println( ttt.a + "   " +  ttt.i )
+	println( ttt.a + "   " +  getactualTypeName(ttt))
 
 }
